c17ee2fae8bab84deb413dd72dbde3ffe08d656e
Som3sh <someshmohanty08@gmail.com>
2026-01-10 12:05:14 +0530

    Add LEFT ANTI JOIN, RIGHT ANTI JOIN, FULL ANTI JOIN, and CROSS JOIN examples

--- Rituals/JOINS.sql
+++ Rituals/JOINS.sql
@@ -94,3 +94,72 @@ ON c.id = o.customer_id
 
 -- Advanced JOINS
 
+-- LEFT ANTI JOIN
+
+-- Get all customers who havent placed any order 
+
+SELECT *
+FROM customers AS c
+LEFT JOIN orders as o
+ON c.id = o.customer_id
+WHERE o.customer_id IS NULL
+
+
+-- RIGHT ANTI JOIN
+
+-- Get all orders without matching customers
+
+SELECT *
+FROM customers AS c
+RIGHT JOIN orders as o 
+ON c.id = o.customer_id
+WHERE c.id IS NULL
+
+
+-- Solve the above task by using only LEFT JOIN
+
+SELECT *
+FROM  orders as o
+LEFT JOIN customers AS c
+ON c.id = o.customer_id
+WHERE c.id IS NULL
+
+
+
+-- FULL ANTI JOIN
+
+-- Find customers without orders and orders without customers 
+
+SELECT *
+FROM customers AS c 
+FULL JOIN orders AS o
+ON c.id = o.customer_id
+WHERE c.id IS NULL OR o.customer_id IS NULL
+
+-- Bonus 
+
+-- Get all customers along with their orders but only for customers who have placed an order 
+-- Without using and INNER JOIN
+
+SELECT *
+FROM customers AS c 
+FULL JOIN orders AS o
+ON c.id = o.customer_id
+WHERE c.id IS NOT NULL AND o.customer_id IS NOT NULL
+
+--or
+
+SELECT *
+FROM customers AS c 
+LEFT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE o.customer_id IS NOT NULL 
+
+-- CROSS JOIN
+-- All possible combinations  -Cartesian Join-
+-- Normally very small amount of scenarios where it can be used 
+-- ,i.e, test data , color combinations for products ,....,
+
+SELECT *
+FROM customers
+CROSS JOIN orders
